32d60ec5e8c746da3ea2664a6758301b3f95a8ba
muogeisen <oliver.geisen@merkurtz.de>
2025-12-08 11:31:24 +0100

    added logging

--- main/main.c
+++ main/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include "driver/ledc.h"
+#include "driver/ledc.h" // LEDC driver
+#include "freertos/FreeRTOS.h" // used for pdMS_TO_TICKS
+#include "freertos/task.h" // used for vTaskDelay
+#include "esp_log.h" // logging
 
+uint8_t led_brightness = 0; // Helligkeit von 0 bis 255
+
+// declare functions
 static void led_init(void);
 static void led_set_brightness(uint8_t brightness);
+static void led_blink(uint8_t times, uint32_t period_ms);
+static void led_fade_off(uint32_t duration_ms);
+static void led_fade_on(uint32_t duration_ms, uint8_t target_brightness);
+static void led_pulse(uint8_t times, uint32_t pulse_duration_ms, uint8_t brightness);
+
+//-------------------------------------------------------------------------------//
+// LED functions
+//-------------------------------------------------------------------------------//
 
 static void led_init(void) {
     ledc_timer_config_t ledc_timer = {
@@ -33,11 +47,71 @@ static void led_set_brightness(uint8_t brightness) {
     uint32_t duty = (brightness * 255) / 255; // Scale brightness to duty cycle
     ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty));
     ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+    led_brightness = brightness;
 }
+
+/**
+ * Let the LED blink a number of times with a given period.
+ * 
+ * @param times Number of blinks
+ * @param period_ms Period in milliseconds
+ */
+static void led_blink(uint8_t times, uint32_t period_ms) {
+    uint8_t last_brightness = led_brightness;
+    uint8_t brightness;
+    if (last_brightness == 0) {
+        brightness = 100;
+    } else {
+        brightness = 0;
+    }
+    for (uint8_t i = 1; i <= times; i += 1) {
+        led_set_brightness(brightness);
+        vTaskDelay(pdMS_TO_TICKS(period_ms));
+        led_set_brightness(last_brightness);
+        vTaskDelay(pdMS_TO_TICKS(period_ms));
+    }
+}
+
+static void led_fade_off(uint32_t duration_ms) {
+    uint8_t start_brightness = led_brightness;
+    for (int i = start_brightness; i >= 0; i--) {
+        led_set_brightness(i);
+        vTaskDelay(pdMS_TO_TICKS(duration_ms / start_brightness));
+    }
+}
+
+static void led_fade_on(uint32_t duration_ms, uint8_t target_brightness) {
+    for (int i = 0; i <= target_brightness; i++) {
+        led_set_brightness(i);
+        vTaskDelay(pdMS_TO_TICKS(duration_ms / target_brightness));
+    }
+}
+
+static void led_pulse(uint8_t times, uint32_t pulse_duration_ms, uint8_t brightness) {
+    for (uint8_t i = 0; i < times; i++) {
+        led_set_brightness(brightness);
+        vTaskDelay(pdMS_TO_TICKS(pulse_duration_ms));
+        led_set_brightness(0);
+        vTaskDelay(pdMS_TO_TICKS(pulse_duration_ms));
+    }
+}
+
+
+//-------------------------------------------------------------------------------//
+// MAIN
+//-------------------------------------------------------------------------------//
+
+static const char* TAG = "LED_STRIP_CTRL";
+
+esp_log_level_set(TAG, ESP_LOG_INFO);
+LOGI(TAG, "ESP32 Xmas Lightstrip Controller started");
 
 void app_main(void)
 {
     led_init();
 
-    led_set_brightness(50); // 50% Helligkeit
+    led_set_brightness(20); // 20% Helligkeit
+    led_blink(10, 500); // 10 mal blinken mit 500ms Intervall
+    led_set_brightness(100); // 100% Helligkeit
+    led_blink(10, 250); // 10 mal blinken mit 500ms Intervall
 }
